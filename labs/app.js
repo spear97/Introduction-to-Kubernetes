67455f5cda6f635ed4f32c01c8a73a1fdd49b102
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-20 15:28:13 -0600

    Update app.js

--- labs/app.js
+++ labs/app.js
@@ -1,11 +1,19 @@
-var express = require('express')
+// Import the express module
+var express = require('express');
+// Import the os module
 var os = require("os");
+// Get the hostname of the system
 var hostname = os.hostname();
-var app = express()
+// Create an instance of the express application
+var app = express();
 
+// Define a route handler for the root URL
 app.get('/', function(req, res) {
-  res.send('Hello world from ' + hostname + '! Your app is up and running!\n')
-})
+  // Send a response with a greeting message containing the hostname
+  res.send('Hello world from ' + hostname + '! Your app is up and running!\n');
+});
+
+// Start the server and listen on port 8080
 app.listen(8080, function() {
-  console.log('Sample app is listening on port 8080.')
-})
+  console.log('Sample app is listening on port 8080.');
+});
